61519a9f8f86b59c28bf3311ab9fd099df637f8c
Bruno Alpachino <brunolalpachino@gmail.com>
2023-10-06 05:27:20 +0300

    nodemon setup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+// create the route
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
